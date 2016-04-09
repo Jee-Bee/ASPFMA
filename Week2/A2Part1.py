7b7b961687ff35a675905162cd9260d7050c10fd
Jee-Bee <gewoon.jeebee@gmail.com>
2016-04-09 10:25:12 +0200

    Sine and Complex Sine

---
+++ Week2/A2Part1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 12:07:32 2016
+
+@author: enjbwink
+"""
+
+import numpy as np
+
+def genSine(A, f, phi, fs, t):
+    """
+    Inputs:
+        A (float) = amplitude of the sinusoid
+        f (float) = frequency of the sinusoid in Hz
+        phi (float) = initial phase of the sinusoid in radians
+        fs (float) = sampling frequency of the sinusoid in Hz
+        t (float) = duration of the sinusoid (is second)
+    Output:
+        The function should return a numpy array
+        x (numpy array) = The generated sinusoid (use np.cos())
+    """
+    # check input values
+    if (A, f, fs >= 0) and (f <= (fs/2)):
+        t_arr = np.arange(0, t, 1/fs)
+        x = A * np.cos(2 * np.pi * f * t_arr + phi)
+        return(x)
+    else:
+        print("One of the functions is a non positive value or `f > fs/2`")
+
+A = 1
+f = 10
+phi = 1
+fs = 50
+t = 0.1
+
+sinewave = genSine(A, f, phi, fs, t)
+print(sinewave)
---
+++ Week2/A2Part2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 12:07:32 2016
+
+@author: enjbwink
+"""
+
+import numpy as np
+
+def genComplexSine(k, N):
+    """
+    Inputs:
+        k (integer) = frequency index of the complex sinusoid
+                      of the DFT
+        N (integer) = length of complex sinusoid in samples
+    Output:
+        The function should return a numpy array
+        cSine (numpy array) = The generated complex sinusoid
+                              (length N)
+    """
+    # check input values
+    if (N, k >= 0) and (k <= (N-1)):
+        # omega = 2 * pi * f == 2 * pi * k
+        # t = n * 1 / fs  => -N/2 ... N/2 -1 = 1/ fs
+        n = np.arange(0, N)
+        cSine = 1 * np.exp(1j * 2 * np.pi * k * n / N)
+        return(cSine)
+    else:
+        print("One of the functions is a non positive value or `k > N-1`")
+
+k = 1
+N = 5
+
+complexsinewave = genComplexSine(k, N)
+print(complexsinewave)
